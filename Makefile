62cbbd7ef323661a63047c7988b1ba85a03ed307
joved@e3541d03 <joel.vedrenne@daveybickfordenaex.com>
2021-03-17 11:09:48 +0100

    Push STM32F103 files.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=arm-none-eabi-gcc
-MACH=cortex-m4
+MACH=cortex-m3
 CFLAGS= -c -mcpu=$(MACH) -mthumb -std=gnu11 -O0 -Wall
 LDFLAGS= -T stm32_ls.ld -nostdlib -Wl,-Map=final.map
 
@@ -17,4 +17,4 @@ clean:
 	rm -rf *.o *.elf *.map
 
 load:
-	openocd -f board/st_nucleo_f4.cfg
+	openocd -f board/bluepill.cfg
--- main.c
+++ main.c
@@ -1,41 +1,28 @@
 #include <stdint.h>
 
-#define RCC   			0x40023800
-#define GPIOA_BASE 		0x40020000
+// register address
+#define RCC_BASE 		0x40021000
+#define GPIOC_BASE 		0x40011000
+#define RCC_APB2ENR 		*(volatile uint32_t *)(RCC_BASE + 0x18)
+#define GPIOC_CRH 		*(volatile uint32_t *)(GPIOC_BASE + 0x04)
+#define GPIOC_ODR 		*(volatile uint32_t *)(GPIOC_BASE + 0x0C)
 
-#define GPIOA_MODER 	*(volatile uint32_t *) (GPIOA_BASE + 0x0)
-#define GPIOA_OTYPER 	*(volatile uint32_t *) (GPIOA_BASE + 0x04)
-#define GPIOA_ODR  		*(volatile uint32_t *) (GPIOA_BASE + 0x14)
-#define GPIOA_BSRR  	*(volatile uint32_t *) (GPIOA_BASE + 0x18)
+// bit fields
+#define RCC_IOPCEN (1<<4)
+#define GPIOC13 (1UL<<13)
 
-#define RCC_AHB1ENR 	*(volatile uint32_t *) (RCC + 0x30)
+int main(void)
+{
+    RCC_APB2ENR |= RCC_IOPCEN;
+    GPIOC_CRH &= 0xFF0FFFFF;
+    GPIOC_CRH |= 0x00200000;
 
-#define PORTA 		(0)		/* 0 = PORTA	*/
-#define LED_PIN    	(5)		/* 5 = PIN5		*/
-
-#define RCC_GPIOAEN 	(1 << PORTA)
-
-int main(){
-
-	//int i_alone;
-	//static int i_value=3;
-	//static int j_alone;
-	//static int jvalue=5;
-	
-	//j_alone=6;
-	
-    RCC_AHB1ENR |= RCC_GPIOAEN;				/* Enable clock on PORTA	*/
-	
-	GPIOA_MODER  &= ~(0x3 << (LED_PIN*2));
-	GPIOA_MODER  |=  (0x1 << (LED_PIN*2));
-	GPIOA_OTYPER &= ~(1 << LED_PIN);
-	
-	GPIOA_ODR |= (1 << LED_PIN);			/* LED is ON				*/
-	
-
-    while(1){
-		GPIOA_ODR ^= (1 << LED_PIN);		/* Toggle the led			*/
+    while(1)
+    {
+        GPIOC_ODR |= GPIOC13;
+        for (int i = 0; i < 500000; i++); // arbitrary delay
 		
-	for (int i = 0; i < 500000; i++) ;		/* Arbitrary delay			*/
-	}
+        GPIOC_ODR &= ~GPIOC13;
+        for (int i = 0; i < 500000; i++); // arbitrary delay
+    }
 }
