04fb0f7eb712a08433035aa289fc2df7518da752
joved@e3541d03 <joel.vedrenne@daveybickfordenaex.com>
2021-03-31 16:32:43 +0200

    Update Startup_file files.

--- stm32f446x/stm32_startup_f446x.c
+++ stm32f446x/stm32_startup_f446x.c
@@ -2,7 +2,7 @@
 #include<stdint.h>
 
 #define SRAM_START  0x20000000U
-#define SRAM_SIZE   (128U * 1024U) 					/* 128 KB */
+#define SRAM_SIZE   (128U * 1024U) 			 /*   128 kb   */
 #define SRAM_END    ((SRAM_START) + (SRAM_SIZE))
 
 #define STACK_START   SRAM_END
