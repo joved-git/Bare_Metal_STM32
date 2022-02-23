78a30a20e0788990c5f2d2e1b700b49e88628744
jvedrenne <jvedrenne@e3541d03>
2022-02-24 00:05:20 +0100

    Remove all useless files.

--- to_f103x.bat
+++
@@ -1,6 +0,0 @@
-rm -f main.c main.h led.c led.h
-
-cp ./stm32f103x/makefile_f103x ./Makefile
-cp ./stm32f103x/stm32_ls_f103x.ld ./stm32_ls.ld
-cp ./stm32f103x/stm32_startup_f103x.c ./stm32_startup.c
-cp ./stm32f103x/main_f103x.c ./main.c
--- to_f407x.bat
+++
@@ -1,8 +0,0 @@
-rm -f main.c main.h led.c led.h
-
-cp ./stm32f407x/makefile_f407x ./Makefile
-cp ./stm32f407x/stm32_ls_f407x.ld ./stm32_ls.ld
-cp ./stm32f407x/stm32_startup_f407x.c ./stm32_startup.c
-
-cp ./stm32f407x/main.* .
-cp ./stm32f407x/led.* .
